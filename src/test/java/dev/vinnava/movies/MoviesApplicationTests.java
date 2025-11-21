e402b7c0e3b308627e962547e3201bbe216fd7f9
Vinnava <109844032+Vinnava@users.noreply.github.com>
2025-11-21 11:12:52 +0530

    Added project

---
+++ src/test/java/dev/vinnava/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinnava.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
